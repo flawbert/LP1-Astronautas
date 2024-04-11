b50522c9b8e2e7294c0a8f64bfbbc0b90039948e
murilocost <muriloalimacosta@hotmail.com>
2024-04-11 16:07:37 -0300

    Fix: Deleção dos arquivos .h e junção de tudo em um único código.

--- lp1-astronauts/main.cpp
+++ lp1-astronauts/main.cpp
@@ -1,7 +1,62 @@
 #include <iostream>
-#include <astronauta.h>
-#include <voo.h>
+#include <list>
+
+using namespace std;
+
+class Astronauta{
+    private:
+    string cpf;
+    string nome;
+    int idade;
+
+    public:
+    Astronauta(const string& cpf, const string& nome, int idade)
+        : cpf(cpf), nome(nome), idade(idade) {}
+
+    std::string getCPF() const {
+        return cpf;
+    }
+    std::string getNome() const {
+        return nome;
+    }
+    int getIdade() const {
+        return idade;
+    }
+};
+
+class Voo {
+private:
+    int codigoVoo;
+    list<string> astronautas;
+
+public:
+    Voo(int codigo) : codigoVoo(codigo) {}
+
+    void adicionarPassageiro(const Astronauta& astronauta) {
+        astronautas.push_back(astronauta.getCPF());
+    }
+};
+
+
 
 int main(void){
+    cout << "Bem vindo ao controle de Voo." << endl;
+
+    cout << "Escolha uma opção:" << endl;
+
+    list<string> opcoes;
+    opcoes.push_back("1 - Cadastrar Astronauta");
+    opcoes.push_back("2 - Cadastrar Voo");
+    opcoes.push_back("3 - Lançar Voo");
+    opcoes.push_back("0 - Sair");
+
+    for (const auto& frase : opcoes) {
+        cout << frase << endl;
+    }
+
+    int op;
+
+    cin >> op;
     
+    return 0;
 }
